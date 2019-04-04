6a06c894334a4ad5cf92a69d510a90b378c513e2
Your Name <minion.ck@gmail.com>
2019-04-04 21:12:32 +0800

    complete done

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+   <div id="app">
+        <product-list-one></product-list-one>
+        <product-list-two></product-list-two>
+   </div>
 </template>
+<script>
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
+    export  default{
+      components:{
+        'product-list-one': ProductListOne,
+        'product-list-two': ProductListTwo
+      },
+      name: 'app',
+      data(){
+        return {
 
+        }
+      }
+    }
+
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  body{
+    font-family: Ubuntu;
+    color:#555;
+  }
 </style>
---
+++ src/components/ProductListTwo.vue
@@ -0,0 +1,49 @@
+<template>
+   <div id="product-list-two">
+       <h2>Product List two</h2>
+       <ul>
+         <li v-for="product in products">
+           <span class="name">{{ product.name }}</span>
+           <span class="price">${{ product.price }}</span>
+         </li>
+       </ul>
+   </div>
+</template>
+
+<script>
+export default {
+  computed:{
+    products(){
+      return this.$store.state.products;
+    },
+    saleProducts(){
+       return this.$store.getters.saleProducts;
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+ #product-list-two{
+   background: #d1e4ff;
+   box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+ }
+ #product-list-two ul{
+   padding: 0;
+   list-style-type:none;
+
+ }
+
+ #product-list-two li{
+   margin-right: 10px;
+   margin-top:10px;
+   padding: 20px;
+   background: rgba(255,255,255,0.7);
+ }
+ .price{
+   font-weight: bold;
+   color:#860ce8;
+   display:block;
+ }
+</style>
